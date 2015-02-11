d42a19b1668f6ea1f694fd4e8ffbcbc5f1bec651
Brent Schneider <bschneider@ewu.edu>
2015-02-11 10:58:52 -0800

    Staring new JS basics course- teamtreehouse.com/library/javascript-basics-2/

--- IntroToProgrammingJS/myscript.js
+++ IntroToProgrammingJS/myscript.js
@@ -1,144 +1,161 @@
 
-/*
-Just some basic javascript code
-  using prompt
-  console.log
-*/ 
-console.log("Hello world");
+// /*
+// Just some basic javascript code
+//   using prompt
+//   console.log
+// */ 
 
-var name = prompt("What is your name")
+// Console - view in browser chrome dev tools > console
+// console.log("Hello world");
+// var name = prompt("What is your name")
 
-alert("Hello " + name);
-
-console.log("The users name is " + name);
-
-
-// If statement
-if (name == "Jim"){
-    alert("Dam it " + name + "! I'm giving it all she got!");
-}
+// Alerts pop up ...
+// alert("Hello " + name);
+// console.log("The users name is " + name);
 
 
-// while statement
-var counter = 12;
-while (counter) {
-  console.log("Hello World");
-  counter = counter -1;
-}
+// // If statement
+// if (name == "Jim"){
+//     alert("Dam it " + name + "! I'm giving it all she got!");
+// }
 
 
-// for statements
-for (counter = 12; counter; counter = counter -1) {
-console.log("Good bye " + name);
-}
+// // while statement
+// var counter = 12;
+// while (counter) {
+//   console.log("Hello World");
+//   counter = counter -1;
+// }
 
 
-var firends = ["Nick", "Bob", "Sally", "Julie", "Frank"];
-
-for (var x = 0; x < firends.length; x+=1 ) {
-    console.log(firends[x]);
-}
-
-
-// Objects store data
-var me = {
-    first_name: "Brent",
-    last_name: "Schneider",
-    "Employee Number": 1
-}
-
-console.log(me.first_name);
-console.log(me.last_name);
-console.log(["Employee Number"]);
-
-console.log(me);
+// // for statements
+// for (counter = 12; counter; counter = counter -1) {
+// console.log("Good bye " + name);
+// }
 
 
-// Functions storing behavior
-// envoke using parentahsies 
+// var firends = ["Nick", "Bob", "Sally", "Julie", "Frank"];
 
-var sayHello = function(){
-
-    var message = "Hello ";
-    message = message + "World!";
-    console.log(message);
-}
-
-sayHello();
+// for (var x = 0; x < firends.length; x+=1 ) {
+//     console.log(firends[x]);
+// }
 
 
-// debug the message
-var debug = function(message) 
-{
-    console.log("debug", message);
-}
+// // Objects store data
+// var me = {
+//     first_name: "Brent",
+//     last_name: "Schneider",
+//     "Employee Number": 1
+// }
 
-var i = 23;
+// console.log(me.first_name);
+// console.log(me.last_name);
+// console.log(["Employee Number"]);
 
-debug(" i has been set to " + i);
-
-var doubleNumber = function(num) 
-{
-    return num * 2;
-}
-
-debug(doubleNumber(7));
+// console.log(me);
 
 
+// // Functions storing behavior
+// // envoke using parentahsies 
 
-// Scope
-// Becareful defining global variables 
+// var sayHello = function(){
 
-var world = "World ";
+//     var message = "Hello ";
+//     message = message + "World!";
+//     console.log(message);
+// }
 
-function nowSayHello() {
-  var hello = "Hello ";
-  // inner function
-  function inner() {
-    var extra = "But wait, there is more!";
-    console.log(hello + world + extra);
-  }
-  inner();
-}
+// sayHello();
 
-nowSayHello();
+
+// // debug the message
+// var debug = function(message) 
+// {
+//     console.log("debug", message);
+// }
+
+// var i = 23;
+
+// debug(" i has been set to " + i);
+
+// var doubleNumber = function(num) 
+// {
+//     return num * 2;
+// }
+
+// debug(doubleNumber(7));
 
 
 
-// Hoisting
-function doSomething(doit) {
-  var color = "blue", 
-      number, 
-      name  = "Jim";
+// // Scope
+// // Becareful defining global variables 
 
-  number = 10;
-  if(doit) {
-    color = "red";
-    number = 10;
-    console.log("Color in if(){}", color);
-  }
-  console.log("Color in if(){}", color);
-}
-doSomething(false);
+// var world = "World ";
 
+// function nowSayHello() {
+//   var hello = "Hello ";
+//   // inner function
+//   function inner() {
+//     var extra = "But wait, there is more!";
+//     console.log(hello + world + extra);
+//   }
+//   inner();
+// }
 
-
-// Shadowing
-
-var meColor = "blue";
-console.log("My Color before My Func()", myColor);
-
-function myFunc () {
-  var myColor = "yellow";
-  console.log("My Color inside My Func()", myColor);
-}
-
-myFunc();
+// nowSayHello();
 
 
 
+// // Hoisting
+// function doSomething(doit) {
+//   var color = "blue", 
+//       number, 
+//       name  = "Jim";
 
-// keep wifi connection
+//   number = 10;
+//   if(doit) {
+//     color = "red";
+//     number = 10;
+//     console.log("Color in if(){}", color);
+//   }
+//   console.log("Color in if(){}", color);
+// }
+// doSomething(false);
+
+
+
+// // Shadowing
+
+// var myColor = "blue";
+// console.log("My Color before My Func()", myColor);
+
+// function myFunc () {
+//   var myColor = "yellow";
+//   console.log("My Color inside My Func()", myColor);
+// }
+
+// myFunc();
+
+
+// Sub Strings
+
+
+// "Hello World"
+// var subWorld = whole.substr(6, 5);
+
+
+
+
+//document.write("<h1>Hello world</h1>");
+
+
+
+alert("hello there");
+document.write("<p>No it works!</p>");
+
+
+
+// BASH keep wifi connection
 // while true; do
 //   echo "$(date -u): \c"  
 //   dig @8.8.8.8 +tries=1 +time=3 google.com | \
