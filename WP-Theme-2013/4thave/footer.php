c2d4b795710c29cde964ec80e8315bd4236bfd63
Brent Schneider <brentschneider@brentschneider.local>
2015-09-27 22:09:33 -0700

    WP responsive theme using parent theme twentythirteen

---
+++ WP-Theme-2013/4thave/footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+			<?php get_sidebar( 'main' ); ?>
+
+
+
+
+				<?php //do_action( 'twentythirteen_credits' ); ?>
+
+			
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+
+</body>
+</html>
